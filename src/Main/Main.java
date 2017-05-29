ccc05bb6a7d7a41030efc285c91cf4e4785b1565
CacTrainer <CacTrainer@DESKTOP-75DNHP5>
2017-05-29 19:29:50 +0200

    Extended Model Started UI implementation

---
+++ src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import UI.LoginWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LoginWindow loginWindow = new LoginWindow();
+        loginWindow.setVisible(true);
+    }
+}
